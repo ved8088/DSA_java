922b3f2fa1ef5134c89d4591d69e6cec4c1b15c7
Vedra <vedraut2200@gmail.com>
2025-06-24 16:13:55 -0400

    Added validparenMinadd.java and cleaned up old class file

---
+++ Stacks_Queue/validparenMinadd.java
@@ -0,0 +1,20 @@
+package Stacks_Queue;
+import java.util.Stack;
+
+public class validparenMinadd {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch == ')'){
+                if(!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                }else{
+                    stack.push(ch);
+                }
+            }else{
+                stack.push(ch);
+            }
+        }
+            return stack.size();
+    }
+}
