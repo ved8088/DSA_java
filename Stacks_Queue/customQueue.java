aadeecf8fe07899e9eb27ef1d1a9e56f25e01b36
Vedra <vedraut2200@gmail.com>
2026-02-24 13:16:42 -0500

    remove the first element and shift all the values to the left

--- Stacks_Queue/customQueue.java
+++ Stacks_Queue/customQueue.java
@@ -9,5 +9,27 @@ public class customQueue {
 
 
 
+        if(isFull()){
+            return false;
+
+        }
+
+        data[end++] = item;
+        return true;
+
+    }
+
+    public int remove() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Queue is empty");
+        }
+        int removed = data[0];
+        //shift the elements to the left here after removing the item
+        for(int i=1;i<end;i++){
+            data[i-1] = data[i];
+        }
+        end--;
+        return removed;
+    }
 
 }
