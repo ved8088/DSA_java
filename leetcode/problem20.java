ef082dbc721a47035b1bcae8dba2950f472b9ff3
Vedra <vedraut2200@gmail.com>
2026-03-09 12:36:33 -0400

    problem 20

--- leetcode/problem20.java
+++ leetcode/problem20.java
@@ -3,7 +3,36 @@ import java.util.Stack;
 
 
 public class problem20 {
+
+
     public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '[' ){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty()){return false;}
+                char top = stack.pop();
+
+                if  ((ch == ')' && top == '(') ||
+                        (ch == '}' && top == '{') ||
+                        (ch == ']' && top == '[')){
+                    continue;
+                } else{
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
 
     }
+
+    public static void main(String[] args) {
+        problem20 sol = new problem20();
+        String s = "()[{()}]";
+        System.out.println(sol.isValid(s));
+    }
+
 }
