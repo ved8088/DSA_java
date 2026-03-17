a15f8f31b485e9b135410603e1ac8fc130289128
Vedra <vedraut2200@gmail.com>
2026-03-17 14:44:32 -0400

    more bst

---
+++ leetcode/bst.java
@@ -0,0 +1,96 @@
+package leetcode;
+
+public class bst {
+    public class Node{
+        private int value;
+        private int height;
+        private Node left;
+        private Node right;
+
+        public Node(int val) {
+            this.value = val;
+        }
+
+        public int getValue(){
+            return value;
+        }
+
+    }
+
+    private Node root;
+
+    public void BST(){
+
+    }
+
+    public int height(Node node){
+        if(node == null){
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void insert(int value){
+        root = insert(value,root);
+    }
+
+        private Node insert(int value,Node node ){
+        if(node == null){
+            node = new Node(value);
+            return node;
+        }
+
+        if(value < node.value){
+            node.left = insert(value, node.left);
+        }
+        if(value > node.value){
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+            return node;
+    }
+
+
+    public void display(){
+        display(this.root,"Root Node");
+    }
+
+   private void display(Node node,String details){
+        if(node == null){
+            return;
+        }
+
+       System.out.println(details + node.getValue() );
+
+        display(node.left, "Left child of " + node.getValue() + " : ");
+       display(node.right, "Right child of " + node.getValue() + " : ");
+   }
+
+   public void populate(int[] nums){
+        for(int i=0;i<nums.length;i++ ){
+            this.insert(nums[i]);
+        }
+   }
+
+   public boolean balanced() {
+        return balanced(root);
+   }
+
+   private boolean balanced(Node node){
+        if(node == null){
+            return true;
+        }
+
+        return Math.abs(height(node.left) - height(node.right)) <= 1 &&
+                        balanced(node.right) && balanced(node.left);
+
+
+   }
+
+}
